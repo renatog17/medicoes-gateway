23c7e4e2c1d20a8d587f1b6f48fde540862b265e
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-12-24 14:45:00 -0300

    project set up

---
+++ src/main/java/com/renato/projects/medicoes_gateway/MedicoesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.renato.projects.medicoes_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicoesGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renato/projects/medicoes_gateway/MedicoesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.projects.medicoes_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
